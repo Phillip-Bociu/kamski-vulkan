833e487563f65551782ed2121b472e6ab024a48c
Phillip-Bociu <dillymcbilly101@gmail.com>
2025-03-26 15:28:52 +0200

    things

--- include/krender.h
+++ include/krender.h
@@ -15,7 +15,6 @@
 #include <cstdint>
 #include <vector>
 #include <span>
-#include <atomic>
 #include <deque>
 #include <functional>
 
@@ -37,184 +36,160 @@
 #endif
 
 #define VK_CHECK(call) \
-	do { \
-		VkResult result = call;\
-		if(result != VK_SUCCESS) {\
-			logError(#call "failed: %d", int(result));\
-			return ReturnCode::UNKNOWN;\
-		}\
-	}while(0)
+    do { \
+        VkResult result = call;\
+        if(result != VK_SUCCESS) {\
+            logError(#call "failed: %d", int(result));\
+            return ReturnCode::UNKNOWN;\
+        }\
+    }while(0)
 
 namespace kvk {
 
-	struct InitSettings {
-		const char* appName;
-		std::uint32_t width;
-		std::uint32_t height;
+    struct InitSettings {
+        const char* appName;
+        std::uint32_t width;
+        std::uint32_t height;
 
 #if !defined(KVK_GLFW)
 #if defined(_WIN32)
-		HWND window;
+        HWND window;
 #endif
 #else
         GLFWwindow* window;
 #endif
-	};
+    };
 
-	struct Pipeline {
-		VkPipelineLayout layout;
-		VkPipeline pipeline;
-	};
+    struct Pipeline {
+        VkPipelineLayout layout;
+        VkPipeline pipeline;
+    };
 
-	struct AllocatedImage {
-		VkImage image;
-		VkImageView view;
-		VmaAllocation allocation;
-		VkExtent3D extent;
-		VkFormat format;
-	};
+    struct AllocatedImage {
+        VkImage image;
+        VkImageView view;
+        VmaAllocation allocation;
+        VkExtent3D extent;
+        VkFormat format;
+    };
 
-	struct AllocatedBuffer {
-		VkBuffer buffer;
-		VmaAllocation allocation;
-		VmaAllocationInfo info;
-	};
+    struct AllocatedBuffer {
+        VkBuffer buffer;
+        VmaAllocation allocation;
+        VmaAllocationInfo info;
+    };
 
-	struct DescriptorAllocator {
-		struct PoolSizeRatio {
-			VkDescriptorType type;
-			float ratio;
-		};
+    struct DescriptorAllocator {
+        struct PoolSizeRatio {
+            VkDescriptorType type;
+            float ratio;
+        };
 
-		void init(VkDevice device,
-				  std::uint32_t initialSets,
-				  std::span<PoolSizeRatio> poolRatios);
+        void init(VkDevice device,
+                  std::uint32_t initialSets,
+                  std::span<PoolSizeRatio> poolRatios);
 
-		void clearPools(VkDevice device);
-		void destroyPools(VkDevice device);
+        void clearPools(VkDevice device);
+        void destroyPools(VkDevice device);
 
-		ReturnCode alloc(VkDescriptorSet& set,
-						 VkDevice device,
-						 VkDescriptorSetLayout layout,
-						 void* pNext = nullptr);
+        ReturnCode alloc(VkDescriptorSet& set,
+                         VkDevice device,
+                         VkDescriptorSetLayout layout,
+                         void* pNext = nullptr);
 
-		VkDescriptorPool getPool(VkDevice device);
-		VkDescriptorPool createPool(VkDevice device,
-									std::uint32_t setCount,
-									std::span<PoolSizeRatio> poolRatios);
+        VkDescriptorPool getPool(VkDevice device);
+        VkDescriptorPool createPool(VkDevice device,
+                                    std::uint32_t setCount,
+                                    std::span<PoolSizeRatio> poolRatios);
 
-		std::vector<PoolSizeRatio> ratios;
-		std::vector<VkDescriptorPool> fullPools;
-		std::vector<VkDescriptorPool> readyPools;
+        std::vector<PoolSizeRatio> ratios;
+        std::vector<VkDescriptorPool> fullPools;
+        std::vector<VkDescriptorPool> readyPools;
 
-		static constexpr std::uint32_t MAX_SETS_PER_POOL = 4096;
+        static constexpr std::uint32_t MAX_SETS_PER_POOL = 4096;
 
-		std::uint32_t setsPerPool;
-	};
+        std::uint32_t setsPerPool;
+    };
 
-	struct SceneData {
-    	glm::mat4 view;
-    	glm::mat4 proj;
-    	glm::mat4 viewproj;
-    	glm::mat4 model;
-    	glm::vec4 ambientColor;
-    	glm::vec4 sunlightDirection;
-    	glm::vec4 sunlightColor;
-	};
+    struct DescriptorWriter {
+        std::deque<VkDescriptorImageInfo> imageInfos;
+        std::deque<VkDescriptorBufferInfo> bufferInfos;
+        std::vector<VkWriteDescriptorSet> writes;
 
-	struct DescriptorWriter {
-		std::deque<VkDescriptorImageInfo> imageInfos;
-		std::deque<VkDescriptorBufferInfo> bufferInfos;
-		std::vector<VkWriteDescriptorSet> writes;
+        void writeImage(int binding,
+                        VkImageView view,
+                        VkSampler sampler,
+                        VkImageLayout layout,
+                        VkDescriptorType type);
+        void writeBuffer(int binding,
+                         VkBuffer buffer,
+                         const std::uint64_t size,
+                         const std::uint64_t offset,
+                         VkDescriptorType type);
 
-		void writeImage(int binding,
-						VkImageView view,
-						VkSampler sampler,
-						VkImageLayout layout,
-						VkDescriptorType type);
-		void writeBuffer(int binding,
-						 VkBuffer buffer,
-						 const std::uint64_t size,
-						 const std::uint64_t offset,
-						 VkDescriptorType type);
+        void clear();
+        void updateSet(VkDevice device,
+                       VkDescriptorSet set);
+    };
 
-		void clear();
-		void updateSet(VkDevice device,
-					   VkDescriptorSet set);
-	};
+    struct PipelineBuilder {
+        PipelineBuilder();
 
-	struct PipelineBuilder {
-		PipelineBuilder();
+        std::vector<VkDynamicState> dynamicState;
+        std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
+        std::vector<VkPushConstantRange> pushConstantRanges;
+        std::vector<VkDescriptorSetLayout> descriptorSetLayouts;
 
-		std::vector<VkDynamicState> dynamicState;
-		std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
-		std::vector<VkPushConstantRange> pushConstantRanges;
-		std::vector<VkDescriptorSetLayout> descriptorSetLayouts;
+        VkFormat colorAttachmentFormat;
 
-		VkFormat colorAttachmentFormat;
+        VkPipelineLayoutCreateInfo layoutCreateInfo;
+        VkPipelineViewportStateCreateInfo viewportState;
+        VkPipelineColorBlendAttachmentState colorBlendAttachment;
+        VkPipelineColorBlendStateCreateInfo blendState;
+        VkPipelineVertexInputStateCreateInfo inputState;
+        VkPipelineDynamicStateCreateInfo dynamicStateInfo;
+        VkPipelineInputAssemblyStateCreateInfo inputAssembly;
+        VkPipelineMultisampleStateCreateInfo multisample;
+        VkPipelineDepthStencilStateCreateInfo depthStencil;
+        VkPipelineRenderingCreateInfo renderInfo;
+        VkPipelineRasterizationStateCreateInfo rasterizer;
 
-		VkPipelineLayoutCreateInfo layoutCreateInfo;
-		VkPipelineViewportStateCreateInfo viewportState;
-		VkPipelineColorBlendAttachmentState colorBlendAttachment;
-		VkPipelineColorBlendStateCreateInfo blendState;
-		VkPipelineVertexInputStateCreateInfo inputState;
-		VkPipelineDynamicStateCreateInfo dynamicStateInfo;
-		VkPipelineInputAssemblyStateCreateInfo inputAssembly;
-		VkPipelineMultisampleStateCreateInfo multisample;
-		VkPipelineDepthStencilStateCreateInfo depthStencil;
-		VkPipelineRenderingCreateInfo renderInfo;
-		VkPipelineRasterizationStateCreateInfo rasterizer;
+        PipelineBuilder& setShaders(VkShaderModule vertexShader, VkShaderModule fragmentShader);
+        PipelineBuilder& setInputTopology(VkPrimitiveTopology topology);
+        PipelineBuilder& setPolygonMode(VkPolygonMode poly);
+        PipelineBuilder& setCullMode(VkCullModeFlags cullMode, VkFrontFace face);
+        PipelineBuilder& setColorAttachmentFormat(VkFormat format);
+        PipelineBuilder& setDepthAttachmentFormat(VkFormat format);
+        PipelineBuilder& setStencilAttachmentFormat(VkFormat format);
 
-		PipelineBuilder& setShaders(VkShaderModule vertexShader, VkShaderModule fragmentShader);
-		PipelineBuilder& setInputTopology(VkPrimitiveTopology topology);
-		PipelineBuilder& setPolygonMode(VkPolygonMode poly);
-		PipelineBuilder& setCullMode(VkCullModeFlags cullMode, VkFrontFace face);
-		PipelineBuilder& setColorAttachmentFormat(VkFormat format);
-		PipelineBuilder& setDepthAttachmentFormat(VkFormat format);
-		PipelineBuilder& setStencilAttachmentFormat(VkFormat format);
+        PipelineBuilder& enableDepthTest(bool depthWriteEnable, VkCompareOp op);
+        PipelineBuilder& enableStencilTest(VkCompareOp compareOp, bool enableWriting);
+        PipelineBuilder& enableBlendingAdditive();
+        PipelineBuilder& enableBlendingAlpha();
 
-		PipelineBuilder& enableDepthTest(bool depthWriteEnable, VkCompareOp op);
-		PipelineBuilder& enableStencilTest(VkCompareOp compareOp, bool enableWriting);
-		PipelineBuilder& enableBlendingAdditive();
-		PipelineBuilder& enableBlendingAlpha();
+        PipelineBuilder& addPushConstantRange(VkShaderStageFlags stage,
+                                              std::uint32_t size,
+                                              std::uint32_t offset = 0);
+        PipelineBuilder& addDescriptorSetLayout(VkDescriptorSetLayout layout);
 
-		PipelineBuilder& addPushConstantRange(VkShaderStageFlags stage,
-											  std::uint32_t size,
-											  std::uint32_t offset = 0);
-		PipelineBuilder& addDescriptorSetLayout(VkDescriptorSetLayout layout);
+        ReturnCode build(Pipeline& pipeline,
+                         const VkDevice device);
+    };
 
-		ReturnCode build(Pipeline& pipeline,
-						 const VkDevice device);
-	};
+    struct FrameData {
+        std::uint32_t swapchainImageIndex;
 
-	struct FrameData {
-	    std::uint32_t swapchainImageIndex;
+        VkFence inFlightFence;
+        VkCommandBuffer commandBuffer;
+        VkSemaphore imageAvailableSemaphore;
+        VkSemaphore renderFinishedSemaphore;
 
-		VkFence inFlightFence;
-		VkCommandBuffer commandBuffer;
-		VkSemaphore imageAvailableSemaphore;
-		VkSemaphore renderFinishedSemaphore;
+        DescriptorAllocator descriptors;
 
-		DescriptorAllocator descriptors;
+        std::vector<std::function<void()>> deletionQueue;
+    };
 
-		std::vector<std::function<void()>> deletionQueue;
-	};
-
-	struct Vertex {
-		glm::vec3 position;
-		float uvX;
-		glm::vec3 normal;
-		float uvY;
-		glm::vec4 color;
-	};
-
-	struct PushConstants {
-        glm::mat4 scaling;
-	    VkDeviceAddress vertexBuffer;
-	    VkDeviceAddress instanceBuffer;
-	};
-
-	struct Mesh {
+    struct Mesh {
 		AllocatedBuffer indices;
 		AllocatedBuffer vertices;
 		VkDeviceAddress vertexBufferAddress;
@@ -230,139 +205,121 @@ namespace kvk {
 		std::vector<GeoSurface> surfaces;
 	};
 
-	enum MaterialPass {
-	    MAT_OPAQUE,
-		MAT_TRANSPARENT,
-		MAT_SHADOW,
-		MAT_COUNT
-	};
+    enum MaterialPass {
+        MAT_OPAQUE,
+        MAT_SHADOW,
+        MAT_COUNT
+    };
 
-	struct MaterialInstance {
-	    Pipeline* pipeline;
-		VkDescriptorSet materialSet;
-		MaterialPass pass;
-	};
+    struct MaterialInstance {
+        Pipeline* pipeline;
+        VkDescriptorSet materialSet;
+        MaterialPass pass;
+    };
 
-	struct RenderObject {
-	    std::uint32_t indexCount;
-	    std::uint32_t firstIndex;
-		VkBuffer indexBuffer;
-		MaterialInstance* materialInstance;
+    struct RenderObject {
+        MaterialInstance* materialInstance;
+        VkBuffer indexBuffer;
+        VkDeviceAddress vertexBufferAddress;
 
-		glm::mat4 transform;
-		VkDeviceAddress vertexBufferAddress;
-	};
+        glm::mat4 transform;
+        std::uint32_t indexCount;
+        std::uint32_t firstIndex;
+    };
 
-	static constexpr std::uint32_t MAX_IN_FLIGHT_FRAMES = 2;
-	struct RendererState {
-		std::atomic<bool> isInitialized;
-		std::vector<std::function<void()>> mainDeletionQueue;
-		std::uint32_t currentFrame;
+    static constexpr std::uint32_t MAX_IN_FLIGHT_FRAMES = 2;
+    struct RendererState {
+        std::uint32_t currentFrame;
 
-		VmaAllocator allocator;
+        VmaAllocator allocator;
 
-		VkInstance instance;
-		VkDevice device;
-		VkPhysicalDevice physicalDevice;
-		std::uint32_t transferFamilyIndex;
-		std::uint32_t graphicsFamilyIndex;
-		std::uint32_t presentFamilyIndex;
-		std::uint32_t computeFamilyIndex;
+        VkInstance instance;
+        VkDevice device;
+        VkPhysicalDevice physicalDevice;
+        std::uint32_t transferFamilyIndex;
+        std::uint32_t graphicsFamilyIndex;
+        std::uint32_t presentFamilyIndex;
+        std::uint32_t computeFamilyIndex;
 
-		VkSampler sampler;
-		VkCommandPool commandPool;
+        VkSampler sampler;
+        VkCommandPool commandPool;
 
-		VkQueue transferQueue;
-		VkQueue graphicsQueue;
-		VkQueue presentQueue;
-		VkQueue computeQueue;
+        VkQueue transferQueue;
+        VkQueue graphicsQueue;
+        VkQueue presentQueue;
+        VkQueue computeQueue;
 
-		VkSurfaceKHR surface;
+        VkSurfaceKHR surface;
 
-		FrameData frames[MAX_IN_FLIGHT_FRAMES];
-		VkCommandBuffer transferCommandBuffers[4];
+        FrameData frames[MAX_IN_FLIGHT_FRAMES];
+        VkCommandBuffer transferCommandBuffers[4];
 
-		AllocatedImage drawImage;
-		AllocatedImage depthImage;
+        AllocatedImage drawImage;
+        AllocatedImage depthImage;
+        AllocatedImage errorTexture;
 
-		AllocatedImage errorTexture;
-
-		SceneData sceneData;
-		VkDescriptorSetLayout sceneDescriptorLayout;
-		VkDescriptorSetLayout samplerDescriptorLayout;
-		//
-		// Swapchain stuff
-		//
-		VkSwapchainKHR swapchain;
-		std::uint32_t swapchainImageCount;
-		std::vector<VkImage> swapchainImages;
-		std::vector<VkImageView> swapchainImageViews;
-		std::vector<VkFramebuffer> framebuffers;
-		VkExtent2D swapchainExtent;
-		VkSurfaceFormatKHR swapchainImageFormat;
-		VkPresentModeKHR swapchainPresentMode;
-	};
+        //
+        // Swapchain stuff
+        //
+        VkSwapchainKHR swapchain;
+        std::uint32_t swapchainImageCount;
+        std::vector<VkImage> swapchainImages;
+        std::vector<VkImageView> swapchainImageViews;
+        VkExtent2D swapchainExtent;
+        VkSurfaceFormatKHR swapchainImageFormat;
+        VkPresentModeKHR swapchainPresentMode;
+    };
 
 
-	ReturnCode init(RendererState& state, const InitSettings* settings);
+    ReturnCode init(RendererState& state, const InitSettings* settings);
 
-	ReturnCode createShaderModuleFromFile(VkShaderModule& shaderModule, RendererState& state, const char* shaderPath);
-	ReturnCode createShaderModuleFromMemory(VkShaderModule& shaderModule, RendererState& state, const std::uint32_t* shaderContents, const std::uint64_t shaderSize);
+    ReturnCode createShaderModuleFromFile(VkShaderModule& shaderModule, RendererState& state, const char* shaderPath);
+    ReturnCode createShaderModuleFromMemory(VkShaderModule& shaderModule, RendererState& state, const std::uint32_t* shaderContents, const std::uint64_t shaderSize);
 
-	FrameData* startFrame(RendererState& state);
+    FrameData* startFrame(RendererState& state, std::uint32_t& frameIndex);
 
-	ReturnCode drawScene(FrameData& frame,
-					     RendererState& state,
-					     const VkExtent2D& extent,
-					     const Pipeline& meshPipeline,
-					     const std::vector<MeshAsset>& meshes);
-
-	ReturnCode endFrame(RendererState& state, FrameData& frame);
+    ReturnCode endFrame(RendererState& state, FrameData& frame);
 
 
-	ReturnCode createSwapchain(RendererState& state,
-							   VkExtent2D extent,
-							   VkSurfaceFormatKHR format,
-							   VkPresentModeKHR presentMode,
-							   std::uint32_t imageCount,
-							   VkSwapchainKHR oldSwapchain = VK_NULL_HANDLE);
+    ReturnCode createSwapchain(RendererState& state,
+                               VkExtent2D extent,
+                               VkSurfaceFormatKHR format,
+                               VkPresentModeKHR presentMode,
+                               std::uint32_t imageCount,
+                               VkSwapchainKHR oldSwapchain = VK_NULL_HANDLE);
 
-	ReturnCode recreateSwapchain(RendererState& state,
-								 const std::uint32_t x,
-								 const std::uint32_t y);
+    ReturnCode recreateSwapchain(RendererState& state,
+                                 const std::uint32_t x,
+                                 const std::uint32_t y);
 
-	ReturnCode createMesh(Mesh& mesh,
-						  RendererState& state,
-						  std::span<std::uint32_t> indices,
-						  std::span<Vertex> vertices);
+    ReturnCode createBuffer(AllocatedBuffer& buffer,
+                            VmaAllocator allocator,
+                            std::uint64_t size,
+                            VkBufferUsageFlags bufferUsage,
+                            VmaMemoryUsage memoryUsage);
 
-	ReturnCode createBuffer(AllocatedBuffer& buffer,
-							VmaAllocator allocator,
-							std::uint64_t size,
-							VkBufferUsageFlags bufferUsage,
-							VmaMemoryUsage memoryUsage);
+    void destroyBuffer(AllocatedBuffer& buffer,
+                       VmaAllocator allocator);
 
-	void destroyBuffer(AllocatedBuffer& buffer,
-					   VmaAllocator allocator);
+    ReturnCode createImage(AllocatedImage& image,
+                           RendererState& state,
+                           const VkFormat format,
+                           const VkExtent3D extent,
+                           const VkImageUsageFlags usageFlags);
 
-	ReturnCode createImage(AllocatedImage& image,
-						   RendererState& state,
-						   const VkFormat format,
-						   const VkExtent3D extent,
-						   const VkImageUsageFlags usageFlags);
+    ReturnCode createImage(AllocatedImage& image,
+                           RendererState& state,
+                           const void* data,
+                           const VkFormat format,
+                           const VkExtent3D extent,
+                           const VkImageUsageFlags usageFlags);
 
-	ReturnCode createImage(AllocatedImage& image,
-						   RendererState& state,
-						   const void* data,
-						   const VkFormat format,
-						   const VkExtent3D extent,
-						   const VkImageUsageFlags usageFlags);
+    void destroyImage(AllocatedImage& image,
+                      VkDevice device,
+                      VmaAllocator allocator);
 
-	void destroyImage(AllocatedImage& image,
-					  VkDevice device,
-					  VmaAllocator allocator);
-
-	ReturnCode loadGltf(std::vector<MeshAsset>& retval,
-						RendererState& state,
-						const char* filePath);
+    ReturnCode createMesh(kvk::Mesh& mesh,
+                          RendererState& state,
+                          std::span<std::uint32_t> indices,
+                          std::span<std::uint8_t> vertices);
 }

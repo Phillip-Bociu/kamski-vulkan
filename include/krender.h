f6a91016f63a05a5934e54c453ad90f3559a1715
Phillip-Bociu <dillymcbilly101@gmail.com>
2026-09-22 07:29:44 +0300

    2 frames in flight babyyyyy

--- include/krender.h
+++ include/krender.h
@@ -47,7 +47,7 @@
     } while(0)
 
 namespace kvk {
-    static constexpr std::uint32_t MAX_IN_FLIGHT_FRAMES = 1;
+    static constexpr std::uint32_t MAX_IN_FLIGHT_FRAMES = 2;
 
     struct InitSettings {
         const char*   appName;
